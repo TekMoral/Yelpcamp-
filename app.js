cd97dbc58cc588dc0c6741283b074d7e98175231
piNfinity <golayiwola081@gmail.com>
2025-02-02 16:30:10 +0100

    Database was changed from yelp-camp to yelpcamp

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const reviewRoutes = require("./routes/reviews");
 //const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = "mongodb://localhost:27017/yelpcamp";
 //if(!dbUrl){
   //console.log("DB_URL not set");   
    //process.exit(1);
